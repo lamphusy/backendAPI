fc1a7ed59c4210d6ae82a0bedd3b096676fc2213
SyLam <lamphusy@gmail.com>
2023-10-02 14:26:52 +0700

    rereshToken

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,4 +1,5 @@
 const { generateToken } = require('../config/jwToken')
+const { generateRefreshToken } = require('../config/refreshToken')
 const User = require('../models/userModel')
 const asyncHandler = require('express-async-handler')
 
@@ -16,9 +17,22 @@ exports.createUser = asyncHandler(async (req, res) => {
 
 //Login a User
 exports.loginUserCtrl = asyncHandler(async (req, res) => {
+    F
     const { email, password } = req.body
     const findUser = await User.findOne({ email })
     if (findUser && (await findUser.isPasswordMatched(password))) {
+        const refreshToken = await generateRefreshToken(findUser?._id)
+        const updateUser = await User.findByIdAndUpdate(
+            findUser.id,
+            {
+                refreshToken: refreshToken,
+            },
+            { new: true }
+        )
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            maxAge: 72 * 60 * 60 * 1000
+        })
         res.json({
             _id: findUser?.id,
             firstname: findUser?.firstname,
@@ -26,6 +40,33 @@ exports.loginUserCtrl = asyncHandler(async (req, res) => {
             email: findUser?.email,
             mobile: findUser?.mobile,
             token: generateToken(findUser?._id),
+        })
+    } else {
+        throw new Error("Invalid Credentials")
+    }
+})
+
+//Log in a Admin
+exports.loginAdmin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const findAdmin = await User.findOne({ email })
+    if (findAdmin.role !== "admin") throw new Error("Not Authorized")
+    if (findAdmin && (await findAdmin.isPasswordMatched(password))) {
+        const refreshToken = await generateRefreshToken(findAdmin?._id)
+        const updateUser = await User.findByIdAndUpdate(
+            findAdmin,
+            {
+                refreshToken: refreshToken,
+            },
+            { new: true }
+        )
+        res.json({
+            _id: findAdmin?.id,
+            firstname: findAdmin?.firstname,
+            lastname: findAdmin?.lastname,
+            email: findAdmin?.email,
+            mobile: findAdmin?.mobile,
+            token: generateToken(findAdmin?._id),
         })
     } else {
         throw new Error("Invalid Credentials")
